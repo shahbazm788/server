fc4fe8ce52546ff258c59c46f1461d3c6705e89b
shahbazm788 <shahbazm788@gmail.com>
2024-07-14 09:24:18 +0500

    update order schema

--- models/orders_model.js
+++ models/orders_model.js
@@ -2,7 +2,22 @@ const mongoose = require("mongoose");
 
 
 const orderSchema = new mongoose.Schema({
+    user_id : {type : mongoose.Schema.Types.ObjectId},
+    user_name:{type: String, require :true},
+    user_email:{type: String},
+    user_phone:{type: String},
+    user_address:{type: String, require :true},
+    orderd_products : [{
+        product_id : mongoose.Schema.Types.ObjectId,
+        qty: Number,
+        produt_price: Number,
+        required:true,
+    }],
+    order_status: {type : String, defalt:"pendding"},
+    payment_status: {type : String},
+    orderd_at : {type:String,required:true},
     
+
 });
 
 
